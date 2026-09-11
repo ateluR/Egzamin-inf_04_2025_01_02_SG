cfc2b560329f897bfb1357eaa0c9b7657d248f82
dup@S0jk! <jackpotcasino72@gmail.com>
2026-09-11 12:06:24 +0200

    Konsolowa z opisem i komentarzem

--- konsolowa/konsolowaab/src/Urzadzenie.java
+++ konsolowa/konsolowaab/src/Urzadzenie.java
@@ -1,4 +1,11 @@
 public class Urzadzenie {
+    /************************************************
+    nazwa: wyswietlKomunikat
+    opis: wyświetla komunikat
+    parametry: komunikat - komunikat wyświetla
+     zwracany typ i opis: brak
+     autor: 000000000000
+     ******************************/
     public void wyswietlKomunikat(String komunikat){
         System.out.println(komunikat);
     }
